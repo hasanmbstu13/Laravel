2e1b13e1a162cf1400fc1c2bb00b47ccbcf02c03
Mainul Hasan <mainul.hasan.cse@gmail.com>
2020-04-26 16:04:53 +0600

    Fetch data from database in various ways

--- laravel_6_basics/app/Http/Controllers/PostsController.php
+++ laravel_6_basics/app/Http/Controllers/PostsController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use DB;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function show($slug)
     {
-    	// Fetch data from the database
-    	$post = DB::table('posts')->where('slug', $slug)->first();
+    	
     	// $posts = [
     	// 	'my-first-post' => 'Hello, this my first blog post',
     	// 	'my-second-post' => 'Now I am getting the hang of this blogging thing.'
@@ -24,8 +25,24 @@ class PostsController extends Controller
     	// 	'post' => $posts[$post] ?? 'Nothing here yet.'
     	// ]);
     	
-    	return view('posts', [
-    		'post' => $post
+    	// Fetch data from the database
+    	// Directly fetch from the database
+    	// $post = DB::table('posts')->where('slug', $slug)->first();
+    	// Fetch data from database using Model
+    	// $post = Post::where('slug', $slug)->first();
+    	// Fetch data, if fail return 404
+    	// $post = Post::where('slug', $slug)->firstOrFail();
+
+    	// if(!$post) {
+    	// 	abort(404);
+    	// }
+
+    	// return view('posts', [
+    	// 	'post' => $post
+    	// ]);
+    	
+    	return view('post', [
+    		'post' => Post::where('slug', $slug)->firstOrFail()
     	]);
     	
     }
