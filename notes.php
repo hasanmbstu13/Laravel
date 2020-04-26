6ffb9f63386f15bbd67be1dffa40018b893a0cc3
Mainul Hasan <mainul.hasan.cse@gmail.com>
2020-04-26 16:13:05 +0600

    Add laravel 6 notes and tricks

---
+++ notes.php
@@ -0,0 +1,16 @@
+<?php
+
+// Laravel 6 basics notes
+
+// To get the help list from artisan write
+# php artisan 
+
+// All configuration setting goes to .env file
+
+// Mac Mysql admin tools
+# --TablePlus --Sequel Pro --Querious
+
+// Model 
+// Useful for 
+		# Fetching data using sql queries
+		# Business logic
