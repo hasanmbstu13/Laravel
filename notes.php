4f74bf63aa570be273066b5dcd0a1fd1e30de50b
Mainul Hasan <mainul.hasan.cse@gmail.com>
2020-05-02 15:19:32 +0600

    Make notes about database migrations and templating

--- notes.php
+++ notes.php
@@ -14,3 +14,23 @@
 // Useful for 
 		# Fetching data using sql queries
 		# Business logic
+
+// Migrations
+	# php artisan make:migration create_posts_table
+
+// Add new column to the table
+// We can do that in two ways
+   #-- Create new migration
+   		# Following is the recommended way when we are in development mode
+		# -- php artisan make:migration add_title_to_posts_table 
+		# Delete all previous database table - it will loss all previous data
+		# -- php artisan migrate:rollback
+		# Then migrate all the migration again
+		# -- php artisan migrate
+		# Dropped all tables and create new tables
+		# -- php artisan migrate:fresh
+	#-- create migration and controller when creating a model
+	#	php artisan make:model Project -mc
+
+// Tinker to interact with the database
+	# php artisan tinker
