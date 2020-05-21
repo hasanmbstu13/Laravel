dd0c9bd6be2a1a7030ce70c73f13fb7ef6aa969e
Mainul Hasan <mainul.hasan.cse@gmail.com>
2020-05-21 15:49:52 +0600

    Fetching data of manay-many relationship

--- laravel_6_basics/resources/views/articles/index.blade.php
+++ laravel_6_basics/resources/views/articles/index.blade.php
@@ -3,12 +3,11 @@
 @section('content')
     <div id="wrapper">
         <div id="page" class="container">
-            @foreach ($articles as $article)
+            {{-- @foreach ($articles as $article) --}}
+            @forelse ($articles as $article)
                 <div class="content">
                     <div class="title">
                         <h2>
-                            {{-- <a href="{{ route('articles.show', $article->id) }}">  --}}
-                            {{-- Or we can use article itself --}}
                             <a href="{{ route('articles.show', $article) }}"> 
                                 {{ $article->title }}
                             </a>
@@ -16,7 +15,9 @@
                         <p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
                         <p>{{ $article->body }}</p>
                 </div>
-            @endforeach
+            @empty
+                <p>No relevant articles</p>
+            @endforelse
         </div>
     </div>    
 @endsection
